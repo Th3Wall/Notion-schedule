ec440281cc8bc5249fdf4aba384008147dcb8ca8
Th3Wall <mandellidavide.95@gmail.com>
2021-07-15 00:49:53 +0200

    Added Notion Client & cleaned index/manifest

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
 
-function App() {
+const App = () => {
+
+  const { Client } = require("@notionhq/client");
+  const databaseId = process.env.REACT_APP_NOTION_API_DATABASE;
+  console.log("databaseId => ", databaseId)
+  const notion = new Client({ auth: process.env.REACT_APP_NOTION_API_KEY });
+  console.log("notion => ", notion);
+
+  const getDatabase = async () => {
+    const response = await notion.databases.query({ database_id: databaseId });
+
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getDatabase();
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
